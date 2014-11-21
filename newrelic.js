1dcb67173f42010fa518f48f9862ec1624433c16
Odd Arild Skaflestad <oaskaflestad@gmail.com>
2014-11-21 16:09:57 +0100

    Initial commit

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['raymon-tour-js'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'c1d81b801f742d028ffe2b3abe4439d21afcfbb5',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
